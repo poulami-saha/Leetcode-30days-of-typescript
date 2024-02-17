06d61d985ea503d3fefc9bf834dff4b8875f4025
Poulami Saha <saha.poulami91@gmail.com>
2024-02-17 09:01:21 +0100

    2627.Debounce

---
+++ Debounce.ts
@@ -0,0 +1,26 @@
+// Given a function fn and a time in milliseconds t, return a debounced version of that function.
+
+// A debounced function is a function whose execution is delayed by t milliseconds 
+//and whose execution is cancelled if it is called again within that window of time. 
+//The debounced function should also receive the passed parameters.
+
+// For example, let's say t = 50ms, and the function was called at 30ms, 60ms, and 100ms. 
+//The first 2 function calls would be cancelled, and the 3rd function call would be executed at 
+//150ms. If instead t = 35ms, The 1st call would be cancelled, the 2nd would be executed at 
+//95ms, and the 3rd would be executed at 135ms.
+
+export type F = (...args: number[]) => void
+
+function debounce(fn: F, t: number): F {
+    let timerId:number;
+    return function (...args) {
+        clearTimeout(timerId);
+        timerId = setTimeout(() => {
+            fn(...args)
+        }, t)
+    }
+}
+// const log = debounce(console.log, 100);
+// log(1); // cancelled
+// log(2); // cancelled
+// log(3); // Logged at t=100ms
